559ee62e19570b961577556238263a462e21b6f3
admin-kazuto <147340489+admin-kazuto@users.noreply.github.com>
2024-07-23 03:25:54 +0700

    firts

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+  const DB_HOST = 'localhost';
+  const DB_USERNAME = 'root';
+  const DB_PASSWORD = "";
+  const DB_NAME = "dbtintonghop";
+  // const BASE_DIR = "/webtintonghop/";
+  const BASE_DIR = "/";
+  const PAGE_SIZE = 10;
+  const NUM_RELATED_POST = 3;
+  const NUM_POST_HOMEPAGE = 20;
+?>
+  
+  
+  
